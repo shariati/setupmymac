11c647dd97b273ab1f96793efec41df117b5f1e4
Amin Shariati <amin.shariati@accenture.com>
2017-07-01 22:51:39 +0800

    Added console colors variables

--- fed.sh
+++ fed.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
-# Black        0;30     Dark Gray     1;30
-# Red          0;31     Light Red     1;31
-# Green        0;32     Light Green   1;32
-# Brown/Orange 0;33     Yellow        1;33
-# Blue         0;34     Light Blue    1;34
-# Purple       0;35     Light Purple  1;35
-# Cyan         0;36     Light Cyan    1;36
-# Light Gray   0;37     White         1;37
 
+# Available Colors to use in Console
 
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BROWN='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT_GRAY='\033[0;37m'
+DARK_GRAY='\033[1;30m'
+LIGHT_RED='\033[1;31m'
+LIGHT_GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LIGHT_BLUE='\033[1;34m'
+LIGHT_PURPLE='\033[1;35m'
+LIGHT_CYAN='\033[1;36m'
+WHITE='\033[1;37m'
+NOCOLOR='\033[0m'
+
+# Check if Tool, Package is available
 
 check_homebrew() {
-  echo Searching for homebrew
+  echo -e "Searching for ${BROWN}homebrew${NOCOLOR}"
   if brew -v > /dev/null; then
     # The homebrew is installed
     echo Homebrew found...
@@ -25,9 +37,12 @@ check_homebrew() {
 }
 
 check_ruby() {
+  echo -e "checking for ruby"
+}
+
+check_oh_my_zsh() {
+  echo -e "checking for Oh My ZSH"
 
 }
 
-check_oh_my_zsh () {
-  
-}
+check_homebrew
